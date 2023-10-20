575de98c139f0ff59cd098a3e760a997a800245d
Ayush Garg <ayush181000@gmail.com>
2023-10-20 18:52:06 +0530

    integrated openai to search in data

--- server.js
+++ server.js
@@ -1,53 +1,79 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { OpenAI } = require("openai");
 
 const shl = require('./model');
 
 require('dotenv').config();
 
+const openai = new OpenAI({
+    api_key: process.env.OPENAI_API_KEY
+});
+
 const app = express();
-// app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(cors({ origin: '*' }));;
 
 app.get('/', async (req, res) => {
     try {
 
-        console.log(req.query);
+        // console.log(req.query);
 
-        const query = {};
+        let query = {};
 
-        if (req.query.projectTechnologies) {
-            query.projectTechnologies = { $regex: req.query.projectTechnologies, $options: 'i' }
+        if (req.query.smartSearch) {
+            const completion = await openai.completions.create({
+                prompt: `Turn this search query into a json search query which can be executed directly in mongodb: ${req.query.smartSearch}. Always give response in a correct json format and do not add anything before and after it.Use only frontend, backend, technologies, infrastructure,name as object keys if required.`,
+                model: "gpt-3.5-turbo-instruct",
+                max_tokens: 200
+            });
+            console.log(completion.choices[0])
+
+            try {
+                query = JSON.parse(completion.choices[0].text);
+            } catch (error) {
+                console.log(error)
+            }
+
         }
 
-        if (req.query.technicalSkillsetFrontend) {
-            query.technicalSkillsetFrontend = { $regex: req.query.technicalSkillsetFrontend, $options: 'i' }
+        if (req.query.projectTitle || query.name) {
+            query.name = { $regex: req.query.projectTitle || query.name, $options: 'i' }
         }
 
-        if (req.query.technicalSkillsetBackend) {
-            query.technicalSkillsetBackend = { $regex: req.query.technicalSkillsetBackend, $options: 'i' }
+        if (req.query.projectTechnologies || query.technologies) {
+            query.technologies = { $regex: req.query.projectTechnologies || query.technologies, $options: 'i' }
         }
 
-        if (req.query.technicalSkillsetDatabases) {
-            query.technicalSkillsetDatabases = { $regex: req.query.technicalSkillsetDatabases, $options: 'i' }
+        if (req.query.technicalSkillsetFrontend || query.frontend) {
+            query.frontend = { $regex: req.query.technicalSkillsetFrontend || query.frontend, $options: 'i' }
         }
 
-        if (req.query.technicalSkillsetInfrastructre) {
-            query.technicalSkillsetInfrastructre = { $regex: req.query.technicalSkillsetInfrastructre, $options: 'i' }
+        if (req.query.technicalSkillsetBackend || query.backend) {
+            query.backend = { $regex: req.query.technicalSkillsetBackend || query.backend, $options: 'i' }
         }
 
-        if (req.query.otherInformationAvailability) {
-            query.otherInformationAvailability = { $regex: req.query.otherInformationAvailability, $options: 'i' }
+        if (req.query.technicalSkillsetDatabases || query.databases) {
+            query.databases = { $regex: req.query.technicalSkillsetDatabases || query.databases, $options: 'i' }
         }
 
-        const data = await shl.find(query);
+        if (req.query.technicalSkillsetInfrastructre || query.infrastructre) {
+            query.infrastructre = { $regex: req.query.technicalSkillsetInfrastructre || query.infrastructre, $options: 'i' }
+        }
 
-        return res.status(200).json(data);
+        if (req.query.otherInformationAvailability || query.availability) {
+            query.availability = { $regex: req.query.otherInformationAvailability || query.availability, $options: 'i' }
+        }
+
+        console.log(query)
+        const jsonData = await shl.find(query);
+
+        return res.status(200).json({ data: jsonData });
     } catch (error) {
-        console.log(error);
-        return res.status(400).send(error.message);
+        console.log(error.message);
+        return res.status(400).send(error);
     }
 
 })
@@ -66,4 +92,3 @@ mongoose
         console.log('database connection failed. Server not started');
         console.error(err);
     });
-
